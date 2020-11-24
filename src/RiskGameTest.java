c37c0ca084280dd3279387701c33d8d979321775
Braxton Martin <braxton_martin@users.noreply.github.com>
2020-11-23 22:14:23 -0500

    Added Tests and added methods to use in Tests

---
+++ src/RiskGameTest.java
@@ -0,0 +1,100 @@
+//package src;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class RiskGameTest {
+
+    RiskGame game;
+
+    @Test
+    public void gameSetup(){
+        game = new RiskGame();
+        assertNotEquals(game,null);
+    }
+
+    @Test
+    public void addPlayers(){
+        game = new RiskGame();
+        ArrayList<Boolean> ai = new ArrayList<Boolean>();
+        game.addPlayer("Braxton");
+        game.addPlayer("Tyler");
+        game.addPlayer("Braden");
+        game.setPlayers(3);
+        ai.add(false);
+        ai.add(false);
+        ai.add(false);
+        game.initializeGame(ai);
+        assertNotEquals(game.getPlayers().size(),0);
+        assertEquals(game.getPlayers().size(),3);
+    }
+
+    @Test
+    public void checkCountriesSetup(){
+        game = new RiskGame();
+        ArrayList<Boolean> ai = new ArrayList<Boolean>();
+        game.addPlayer("Braxton");
+        game.addPlayer("Tyler");
+        game.addPlayer("Braden");
+        game.setPlayers(3);
+        ai.add(false);
+        ai.add(false);
+        ai.add(false);
+        game.initializeGame(ai);
+        assertNotEquals(game.getCountries(),null);
+    }
+
+    @Test
+    public void checkAdjacentSetup(){
+        game = new RiskGame();
+        ArrayList<Boolean> ai = new ArrayList<Boolean>();
+        game.addPlayer("Braxton");
+        game.addPlayer("Tyler");
+        game.addPlayer("Braden");
+        game.setPlayers(3);
+        ai.add(false);
+        ai.add(false);
+        ai.add(false);
+        game.initializeGame(ai);
+        for(Country c : game.getCountries()){
+            assertNotEquals(c.getAdjacents(),null); //All countries should have at least 1 adjacent
+        }
+    }
+
+    @Test
+    public void checkNextTurn(){
+        game = new RiskGame();
+        ArrayList<Boolean> ai = new ArrayList<Boolean>();
+        game.addPlayer("Braxton");
+        game.addPlayer("Tyler");
+        game.addPlayer("Braden");
+        game.setPlayers(3);
+        ai.add(false);
+        ai.add(false);
+        ai.add(false);
+        game.initializeGame(ai);
+        Player p1 = game.getCurrentPlayer(); //Get Player 1
+        game.nextTurn();
+        Player p2 = game.getCurrentPlayer(); //Get Player 2
+        assertNotEquals(p1,p2); //Player 1 and Player 2 are not the same
+    }
+
+    @Test
+    public void getAttackableCountriesTest(){
+        game = new RiskGame();
+        ArrayList<Boolean> ai = new ArrayList<Boolean>();
+        game.addPlayer("Braxton");
+        game.addPlayer("Tyler");
+        game.addPlayer("Braden");
+        game.setPlayers(3);
+        ai.add(false);
+        ai.add(false);
+        ai.add(false);
+        game.initializeGame(ai);
+        String[] attackableCountries = game.getAttackableCountries(game.getCountries().get(0).getName());
+        assertNotEquals(attackableCountries,null);
+    }
+}
